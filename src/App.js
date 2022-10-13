e6b7a3909c28f7287436e09865969d422e5596ed
patrick mugambi <patrick@kali.kali>
2022-10-13 19:41:40 +0300

    add chanege date value using state

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Calendar from 'react-calendar/dist/umd/Calendar';
 import 'react-calendar/dist/Calendar.css';
+import React, { useState } from 'react';
+import moment from 'moment';
 function App() {
+  const [dateState, setDateState] = useState(new Date());
+  const changeDate = (e) => setDateState(e);
   return (
     <div className="App">
-  <Calendar/>
+  <Calendar value={dateState} onChange={changeDate}/>
    <h1>Calendar</h1>
     </div>
   );
