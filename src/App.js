f6b21e9f13fd889a21c5535bbb88c7f420da818d
patrick mugambi <patrick@kali.kali>
2022-10-14 04:08:24 +0300

    final changes

--- src/App.js
+++ src/App.js
@@ -9,11 +9,19 @@ function App() {
   const changeDate = (e) => setDateState(e);
   return (
     <div className="App">
-      <div className='container'>
+     
+<div class="container-fluid p-10 ">
+<div class="row">
+  <div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-2 gap-1">
+<div>
   <Calendar value={dateState} onChange={changeDate}/>
-  <p>Current selected date is <b>{moment(dateState).format('MMMM Do YYYY')}</b></p>
+  </div>
+  <div>
+  <h1 className='text-4xl display-4'>Current selected date is <b>{moment(dateState).format('MMMM Do YYYY')}</b></h1>
+  </div>
 </div>
-   <h1>Calendar</h1>
+</div>
+</div>
     </div>
   );
 }
